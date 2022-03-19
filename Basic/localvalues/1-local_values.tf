0d7a32eb6f81e459878908d189f0edecd4ee50e4
vidyadharreddy18 <vidyadhar.bth@gmail.com>
2022-03-19 18:30:54 -0400

    add functions example, modified local_values example

--- Basic/localvalues/1-local_values.tf
+++ Basic/localvalues/1-local_values.tf
@@ -9,12 +9,10 @@ resource "aws_instance" "instance-1" {
     ami = "ami-0c02fb55956c7d316"
     instance_type = "t2.micro"
     tags = local.common_tags
-  
 }
 
 resource "aws_instance" "instance-2" {
     ami = "ami-0c02fb55956c7d316"
     instance_type = "t2.micro"
     tags = local.common_tags
-  
 }
