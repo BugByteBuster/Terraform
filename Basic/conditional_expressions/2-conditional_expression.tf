d41ebab9c8d36d19bcb5c7699c26803f5a8e8d5e
vidyadharreddy18 <vidyadhar.bth@gmail.com>
2022-03-19 15:33:14 -0400

    add ami and instance type

--- Basic/conditional_expressions/2-conditional_expression.tf
+++ Basic/conditional_expressions/2-conditional_expression.tf
@@ -1,13 +1,13 @@
 resource "aws_instance" "dev_instance" {
-    ami = ""
-    instance_type = ""
+    ami = "ami-0c02fb55956c7d316"
+    instance_type = "t2.micro"
     count = var.dev_r_prod == "dev" ? 1 : 0
   
 }
 
 resource "aws_instance" "prod_instance" {
-    ami = ""
-    instance_type = ""
+    ami = "ami-0c02fb55956c7d316"
+    instance_type = "t2.micro"
     count = var.dev_r_prod == "prod" ? 1 : 0
   
 }
