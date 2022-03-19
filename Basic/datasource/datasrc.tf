66c0b2bebed49003ac6577f4b819551b3b4563ef
vidyadharreddy18 <vidyadhar.bth@gmail.com>
2022-03-19 19:05:11 -0400

    format

--- Basic/datasource/datasrc.tf
+++ Basic/datasource/datasrc.tf
@@ -1,13 +1,13 @@
 data "aws_ami" "ami_id" {
-    most_recent = true
-    owners = ["amazon"]
-    filter {
-      name = "name"
-      values = ["amzn2-ami-hvm*"]
-    }
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
 }
 
 resource "aws_instance" "my-instance" {
-    ami = data.aws_ami.ami_id.id
-    instance_type = "t2.micro"
+  ami           = data.aws_ami.ami_id.id
+  instance_type = "t2.micro"
 }
