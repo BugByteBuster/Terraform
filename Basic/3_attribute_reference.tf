f0ad5f531c97f800a1432747b43fd2f516272db5
vidyadharreddy18 <vidyadhar.bth@gmail.com>
2022-03-18 23:23:55 -0400

    add attribute reference: referencing eip in ec2

---
+++ Basic/3_attribute_reference.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  secret_key = "ItGgUUKtJo6GV8SadXps6cx3b9jHTn94bNy+cWAj"
+  access_key = "AKIAVOZEWIATG7PIR4WY"
+}
+
+resource "aws_instance" "tf-instance" {
+    ami = "ami-0c02fb55956c7d316"
+    instance_type = "t2.micro"
+  
+}
+
+resource "aws_eip" "tf-eip" {
+    vpc = true
+  
+}
+
+resource "aws_eip_association" "tf-association" {
+    instance_id = aws_instance.tf-instance.id
+    allocation_id = aws_eip.tf-eip.id
+  
+}
