97ee096c4c802fde57e9e390576bc2ded5c81880
vidyadharreddy18 <vidyadhar.bth@gmail.com>
2022-03-19 17:54:00 -0400

    add local values

---
+++ Basic/localvalues/1-local_values.tf
@@ -0,0 +1,20 @@
+locals {
+  common_tags = {
+      Owner = "DevOps Team"
+      Service = "Backend"
+  }
+}
+
+resource "aws_instance" "instance-1" {
+    ami = "ami-0c02fb55956c7d316"
+    instance_type = "t2.micro"
+    tags = local.common_tags
+  
+}
+
+resource "aws_instance" "instance-2" {
+    ami = "ami-0c02fb55956c7d316"
+    instance_type = "t2.micro"
+    tags = local.common_tags
+  
+}
