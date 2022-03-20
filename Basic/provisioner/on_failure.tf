6b30e0d895ad62aba5cc6b077588ec791e02cc08
vidyadharreddy18 <vidyadhar.bth@gmail.com>
2022-03-20 00:45:19 -0400

    add example for on failure usecase

--- Basic/provisioner/on_failure.tf
+++ Basic/provisioner/on_failure.tf
@@ -9,31 +9,18 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  egress {
-    description = "Outbound Allowed"
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
-
-resource "aws_instance" "myec_instance" {
+resource "aws_instance" "myec2" {
   ami                    = "ami-0b1e534a4ff9019e0"
   instance_type          = "t2.micro"
   key_name               = "ec2-key"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   provisioner "remote-exec" {
+    on_failure = continue
     inline = [
       "sudo yum -y install nano"
-    ]
-  }
-  provisioner "remote-exec" {
-    when = destroy
-    inline = [
-      "sudo yum -y remove nano"
     ]
   }
   connection {
