861cd7a0e4b57e15ac24c6b16698be3d75019a53
vidyadharreddy18 <vidyadhar.bth@gmail.com>
2022-03-20 09:50:01 -0400

    module usage

---
+++ Basic/registry/registry-ec2.tf
@@ -0,0 +1,15 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  for_each = toset(["one", "two", "three"])
+  name = "instance-${each.key}"
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "t2.micro"
+  key_name               = "ur_key"
+  
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
