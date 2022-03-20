2e122a32e05d97e27ed65adac0a2a58183c2a0fc
vidyadharreddy18 <vidyadhar.bth@gmail.com>
2022-03-20 10:24:56 -0400

    workspace

---
+++ Basic/workspace/ec2.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "ec2-instance" {
+  ami           = ""
+  instance_type = lookup(var.instance_type, terraform.workspace)
+
+}
+
+variable "instance_type" {
+  type = map(any)
+  default = {
+    dev   = "t2.nano"
+    stage = "t2.micro"
+    prod  = "t2.medium"
+  }
+
+}
