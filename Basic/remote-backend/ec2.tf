8234dead5a977fa4cf5a6206f09f7e72a28bbe4a
vidyadharreddy18 <vidyadhar.bth@gmail.com>
2022-03-20 12:10:05 -0400

    changes

--- Basic/remote-backend/ec2.tf
+++ Basic/remote-backend/ec2.tf
@@ -1,4 +1,4 @@
 resource "aws_instance" "ec2-aws_instance" {
-    ami = "ami-0c02fb55956c7d316"
-    instance_type = "t2.micro"
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
 }
