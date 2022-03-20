ddbca4eccff933b55413dd2f6c1191d0c5a5afd4
vidyadharreddy18 <vidyadhar.bth@gmail.com>
2022-03-20 01:19:58 -0400

    add DRY modular aspects

---
+++ Basic/DRY/modules/ec2/main.tf
@@ -0,0 +1,4 @@
+resource "aws_instance" "name" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+}
--- Basic/DRY/modules/ec2/provider.tf
+++ Basic/DRY/modules/ec2/provider.tf
@@ -10,9 +10,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-
-resource "aws_instance" "tf-instance" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
-}
