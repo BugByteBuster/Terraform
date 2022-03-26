b364e7f62a7e745b48523b8d80a9ce12594f735b
vidyadharreddy18 <vidyadhar.bth@gmail.com>
2022-03-26 09:03:37 -0400

    minor modification

--- krd-terraform/trfm-settings-providers-resources/my-ec2.tf
+++ krd-terraform/trfm-settings-providers-resources/my-ec2.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "my-ec2" {
+resource "aws_instance" "my-ec2-1" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
-  user_data = file("${path.module}/app-install.sh")
+  user_data     = file("${path.module}/app-install.sh")
   tags = {
-    "name" = "my-ec2"
+    "name" = "my-ec2-2"
   }
 }
